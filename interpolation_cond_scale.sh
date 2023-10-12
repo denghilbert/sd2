32ec4ca080f7f5acca94356f569b55b5c6892b3e
denghilbert <971431670@qq.com>
2023-10-12 01:00:01 -0400

    Add condition scale interpolation shell

---
+++ interpolation_cond_scale.sh
@@ -0,0 +1,15 @@
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/happ_dog --n_samples 1 --prompt "a happy dog"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/cute_dog --n_samples 1 --prompt "a cute dog"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/nice_car --n_samples 1 --prompt "a nice car"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/big_house --n_samples 1 --prompt "a big house"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/old_car --n_samples 1 --prompt "an old car"
+
+
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/a_dog --n_samples 1 --prompt "a dog"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/a_cat --n_samples 1 --prompt "a cat"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/a_kid --n_samples 1 --prompt "a kid"
+
+
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/a_black_wallet --n_samples 1 --prompt "a black wallet"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/human_sitting_classroom --n_samples 1 --prompt "a human sitting in the classroom"
+python scripts/txt2img.py --ckpt ./checkpoints/v2-1_768-ema-pruned.ckpt --config configs/stable-diffusion/v2-inference-v.yaml --H 768 --W 768  --device cuda --outdir ./test/interpolation_cond_scale/astro_horse --n_samples 1
